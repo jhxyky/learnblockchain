c3d1248cf1d88ddef19cabf4aeff70950b6aadb7
jiaohaoxiang <jiaohaoxiang@pengzhirui.com>
2025-05-29 17:50:27 +0800

    chore: cleanup incorrectly generated files

--- src/interfaces/IAToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-import "openzeppelin/token/ERC20/IERC20.sol";
-
-interface IAToken is IERC20 {
-    function balanceOf(address user) external view returns (uint256);
-} 
